6cc598e626a11998d9c6bdad5f19acdf748e09c4
umuta <umut.kamchybekkyzy@gmail.com>
2021-11-20 11:34:33 -0600

    Techtorial practice

---
+++ src/Primitives/CastingPractice.java
@@ -0,0 +1,34 @@
+package Primitives;
+
+public class CastingPractice {
+    public static void main(String[] args) {
+
+        int number1 = 23;
+        long l1= number1;// implicit casting happens by java
+
+        short sh1 = (short)number1; //explicit casting
+
+        byte by1 = 1;
+
+        byte by2 = 2;
+
+      /* short sum1 = by1+by2; compile time error (due ti numeric promotion
+        java wants to store the results in to larger data type (int)
+        thats why it is not compiling)*/
+
+        short sum2 = (short)(by1+by2);
+
+        //short sum3 = (short)by1+ (short)by2; this is still individual casting for each variable result is not casting yet
+
+        int price = (int)750.99;
+        System.out.println(price);
+
+        double samsung = 900.45;
+        int priceSamsung = (int)samsung;
+        System.out.println(samsung);
+        System.out.println(priceSamsung);
+
+
+
+    }
+}
--- src/Primitives/CompoundAssignments.java
+++ src/Primitives/CompoundAssignments.java
@@ -30,6 +30,47 @@ public class CompoundAssignments {
         System.out.println(num2);
 
 
+        //*=
+
+        num1 = 4;
+        num1*=5;
+
+        System.out.println(num1);//20
+
+        // /=
+
+        num1/=4;
+        System.out.println(num1);//5
+
+        //%=
+
+        num1 %=2;
+        System.out.println(num1);//remainder 1
+
+        // boolean bl1 = true; you cannot compound
+        //bl1+= false;
+
+        char ch1='a';
+        char ch2 = 'm';
+        char ch3 = 't';
+
+
+        ch3+= 'a';
+        System.out.println("This is ch3>>" +ch3); //
+
+        ch2 *='3';
+        System.out.println(ch2);//ᖷ
+
+        ch1 -=9;
+        System.out.println(ch1);//X
+
+        ch1%=2;
+        System.out.println(ch1);//
+
+
+
+
+
     }
 
 }
